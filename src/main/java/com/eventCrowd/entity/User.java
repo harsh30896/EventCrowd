cd8771041be3869b495cdf76150ec375bd0f556e
Harsh Pandey <hars30896@gmail.com>
2024-10-31 19:21:25 +0530

    react added for frontend and now it can work directly

--- src/main/java/com/eventCrowd/entity/User.java
+++ src/main/java/com/eventCrowd/entity/User.java
@@ -24,6 +24,7 @@ public class User {
     private String name;
     private String email;
     private String password;
+    private String location;
 
     @Enumerated(EnumType.STRING)
     private Role role;
